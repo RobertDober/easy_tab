de4b0f2035333a38d7aa6c240cc4478a864138b3
RobertDober <robert.dober@gmail.com>
2022-08-28 10:04:21 +0200

    Mix project scaffolded

---
+++ lib/easy_tab.ex
@@ -0,0 +1,18 @@
+defmodule EasyTab do
+  @moduledoc """
+  Documentation for `EasyTab`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EasyTab.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/easy_tab_test.exs
@@ -0,0 +1,8 @@
+defmodule EasyTabTest do
+  use ExUnit.Case
+  doctest EasyTab
+
+  test "greets the world" do
+    assert EasyTab.hello() == :world
+  end
+end
